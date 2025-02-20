991d34aeaae2604b07565dd6e7f5e9cbdbef0b98
baguajoe <baguajoe@gmail.com>
2025-02-20 20:30:07 +0000

    updated layout

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,8 +1,12 @@
-import React from "react";
-import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 import HomePage from "./pages/home";
+import LeftSidebar from "./component/LeftSidebar";
+import RightSidebar from "./component/RightSidebar";
+import Messaging from "./component/Messaging";
+
 // import Login from "./pages/Login";
 // import Signup from "./pages/Signup";
 // import ForgotPassword from "./pages/ForgotPassword";
@@ -14,12 +18,38 @@ import HomePage from "./pages/home";
 // import ContactUs from "./pages/ContactUs";
 
 
+const PageLayout = () => {
+    return (
+        <div className="container-fluid mt-3">
+            <div className="row">
+                {/* Left Sidebar */}
+                <div className="col-md-3 d-none d-md-block">
+                    <LeftSidebar />
+                </div>
+
+                {/* Main Content */}
+                <div className="col-md-6">
+                    <Outlet /> {/* This is where route components will render */}
+                </div>
+
+                {/* Right Sidebar */}
+                <div className="col-md-3 d-none d-md-block">
+                    <RightSidebar />
+                </div>
+            </div>
+        </div>
+    );
+};
+
 const Layout = () => {
+    const [showMessaging, setShowMessaging] = useState(false)
+
     return (
         <Router>
             <Navbar />
-            <div className="content-wrapper">
-                <Routes>
+            {/* <div className="content-wrapper"> */}
+            <Routes>
+                <Route element={<PageLayout />}>
                     <Route element={<HomePage />} path="/" />
                     {/* <Route element={<Login />} path="/login" />
                     <Route element={<Signup />} path="/signup" />
@@ -43,8 +73,11 @@ const Layout = () => {
                             </React.Fragment>
                         }
                     />
-                </Routes>
-            </div>
+                </Route>
+            </Routes>
+            {/* </div> */}
+            {/* Floating Chat Panel */}
+            {showMessaging && <Messaging closeChat={() => setShowMessaging(false)} />}
             <Footer />
         </Router>
     );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,41 +1,30 @@
 import React, { useState } from "react";
-import LeftSidebar from "../component/LeftSidebar";
+
 import Feed from "../component/Feed";
-import RightSidebar from "../component/RightSidebar";
-import Messaging from "../component/Messaging";
 import JobListings from "../component/JobListings";
 import Tabs from "../component/Tabs";
 
 const HomePage = () => {
     const [activeTab, setActiveTab] = useState("home");
-    const [showMessaging, setShowMessaging] = useState(false);
 
     return (
         <>
         
             <div className="container-fluid mt-3">
                 <div className="row">
-                    {/* Left Sidebar (Profile + Quick Links) */}
-                    <div className="col-md-3 d-none d-md-block">
-                        <LeftSidebar />
-                    </div>
 
                     {/* Main Content */}
-                    <div className="col-md-6">
+                    <div className="col">
                         <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
                         {activeTab === "home" && <Feed />}
                         {activeTab === "jobs" && <JobListings />}
                     </div>
 
-                    {/* Right Sidebar (Suggestions + Ads) */}
-                    <div className="col-md-3 d-none d-md-block">
-                        <RightSidebar />
-                    </div>
+                   
                 </div>
             </div>
 
-            {/* Floating Chat Panel */}
-            {showMessaging && <Messaging closeChat={() => setShowMessaging(false)} />}
+          
         </>
     );
 };
